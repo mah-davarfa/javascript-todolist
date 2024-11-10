c05543909a137e33527cf8f6875afc2ef7f1a5ef
Mah-Davarfa <mah.davarfa@gmail.com>
2024-11-10 13:12:56 -0500

    the array of joke added

--- todolist.js
+++ todolist.js
@@ -1,4 +1,9 @@
 const randomeJoke = ()=>{
-
+    const jokes = [
+        "Why did the scarecrow win an award? Because he was outstanding in his field!",
+        "I told my computer I needed a break, and now it won’t stop sending me coffee memes!",
+        "Why don’t skeletons fight each other? They don’t have the guts!",
+        "I tried to catch some fog, but I mist!"
+    ];
 }
 randomeJoke();
