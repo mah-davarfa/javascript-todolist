73c7d7d955dec9d78d2cf0f9a90ae5589250559a
mah-davarfa <mah.davarfa@gmail.com>
2024-11-10 20:41:41 -0500

    array of astrology added

--- todolist.js
+++ todolist.js
@@ -5,5 +5,11 @@ const randomeJoke = ()=>{
         "Why don’t skeletons fight each other? They don’t have the guts!",
         "I tried to catch some fog, but I mist!"
     ];
+    const astrology = [
+        "Today is a great day to start something new, but don’t forget your wallet!",
+        "Your patience will pay off soon, just like your phone bill!",
+        "The stars say it’s time to be bold, just like your font settings!",
+        "An unexpected journey may be in your future. Don’t forget the GPS!"
+    ];
 }
 randomeJoke();
